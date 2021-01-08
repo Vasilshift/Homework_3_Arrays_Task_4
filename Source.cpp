70ae3e40fc982d92d397a1a00a336bd2ea1129b9
Vasilshift <Vasilshift@github.com>
2021-01-08 16:10:41 +0500

    Work variant !!!

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,8 @@ int main() {
 	int i;
 	
 	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int arr1[] = { 0,0,0,0,0,0,0,0,0,0 };
+	int arr2[10];
 
 	cout << "Income array: " << endl;
 
@@ -16,22 +18,27 @@ int main() {
 
 	}
 	cout << endl;
-	
+	cout << endl;
+
 	cout << "Array elements from 1 to 5: " << endl;
 
 	for (i = 0; i < 5; i++) {
+		
+		arr2[i] = arr1[i] + arr[i];
 
-		cout << arr[i] << " ";
+		cout << arr2[i] << " ";
 				
 	}
 
 	cout << endl;
 	
-	cout << "Array elements from 5 to 10: " << endl;
+	cout << "Array elements from 6 to 10: " << endl;
 
 	for (i = 5; i < 10; i++) {
 
-		cout << arr[i] <<" ";
+		arr2[i] = arr1[i] + arr[i];
+		
+		cout << arr2[i] <<" ";
 
 	}
 	
